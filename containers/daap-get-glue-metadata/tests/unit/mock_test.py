c4d294da8975e4fd6ac8452b1ceac1cd61433a49
Mat Moore <MatMoore@users.noreply.github.com>
2023-09-04 15:40:14 +0100

    Add workflow task to run the unit tests

--- containers/daap-get-glue-metadata/tests/unit/mock_test.py
+++ containers/daap-get-glue-metadata/tests/unit/mock_test.py
@@ -1,9 +1,6 @@
 import json
-from moto import mock_glue
 import boto3
-import pytest
 import get_glue_metadata
-import json
 
 TEST_DATABASE_NAME = 'test_db'
 
@@ -23,15 +20,15 @@ def glue_table_input(name, location):
             'Location': location,
             'BucketColumns': [],
             'Compressed': False,
-            'InputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat',
+            'InputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat',  # noqa: E501
             'NumberOfBuckets': -1,
-            'OutputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat',
+            'OutputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat',  # noqa: E501
             'Parameters': {},
             'SerdeInfo': {
                 'Parameters': {
                     'serialization.format': '1'
                 },
-                'SerializationLibrary': 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+                'SerializationLibrary': 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'  # noqa: E501
             },
             'SkewedInfo': {
                 'SkewedColumnNames': [],
@@ -45,29 +42,18 @@ def glue_table_input(name, location):
     }
 
 
-@pytest.fixture
-def glue_client():
-    """
-    Create a mock glue catalogue containing one table.
-    """
-    with mock_glue():
-        client = boto3.client('glue', region_name='us-east-1')
-        
-        client.create_database(
-            DatabaseInput={
-                'Name': TEST_DATABASE_NAME
-            }
-        )
-
-        yield client
-
-
-def test_success_case(glue_client, monkeypatch):
+def test_success_case(glue_client, monkeypatch, fake_context):
+    test_database_name = 'test_db'
     test_table_name = 'test_table'
-    s3_location = f's3://my-bucket/{TEST_DATABASE_NAME}/{test_table_name}'
+    s3_location = f's3://my-bucket/{test_database_name}/{test_table_name}'
     table_input = glue_table_input(test_table_name, s3_location)
+    glue_client.create_database(
+        DatabaseInput={
+                'Name': test_database_name
+        }
+    )
     glue_client.create_table(
-        DatabaseName=TEST_DATABASE_NAME,
+        DatabaseName=test_database_name,
         TableInput=table_input
     )
 
@@ -76,14 +62,15 @@ def test_success_case(glue_client, monkeypatch):
 
     event = {
         "queryStringParameters": {
-            "database": TEST_DATABASE_NAME,
+            "database": test_database_name,
             "table": test_table_name
         },
     }
-    test_response = get_glue_metadata.handler(event, context={})
+    test_response = get_glue_metadata.handler(event, context=fake_context)
     body = json.loads(test_response['body'])
 
     assert test_response['statusCode'] == 200
     assert body['Table'].items() >= table_input.items()
-    assert body['ResponseMetadata'].keys() == {'HTTPStatusCode', 'HTTPHeaders', 'RetryAttempts'}
-    
+
+    expected_metadata = {'HTTPStatusCode', 'HTTPHeaders', 'RetryAttempts'}
+    assert body['ResponseMetadata'].keys() == expected_metadata
